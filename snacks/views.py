f8d428107b6105859b9b9d24ed9a5611f496ccc1
Cole Gibbs <colegibbs0@gmail.com>
2022-05-24 15:17:57 -0700

    created snack data and snack list view

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snacks
 
-# Create your views here.
+class SnackListView(ListView):
+  template_name = 'snack_list.html'
+  model = Snacks
